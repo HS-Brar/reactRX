8cadecd6988c4bbd24eb7898fceb2fa0fb403152
HS-Brar <92150618+HS-Brar@users.noreply.github.com>
2024-05-21 14:36:31 +0530

    Add files via upload

---
+++ extra/index.jsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import { Box, Grid, useTheme } from "@mui/material";
+import { Table, TableBody, TableCell, TableContainer, TableRow, TableHead, Paper } from '@mui/material';
+import { styled } from '@mui/system';
+import { tokens } from "../../theme";
+import Header from "../../components/Header";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    fontWeight: 'bold',
+    padding: theme.spacing(1),
+    border: '1px solid black', // Adding border to the table cell
+    textAlign: "center",
+    backgroundColor: "#FF612B"
+}));
+
+const CustomTableHeaderCell = styled(TableCell)(({ theme }) => ({
+    padding: theme.spacing(1),
+    border: '1px solid black', // Adding border to the table cell
+    textAlign: "center",
+    backgroundColor: "#FAF8F2"
+}));
+const CustomTableCell = styled(TableCell)(({ theme }) => ({
+    padding: theme.spacing(1),
+    border: '1px solid black', // Adding border to the table cell
+    textAlign: "center",
+    backgroundColor: "#D3D3D3"
+
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    //borderBottom: '1px solid black', // Optional: adding bottom border to the row
+}));
+
+const EXTRA = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    const data = [
+        { name: 'Ashu', age: 16 },
+        { name: 'John', age: 25 }
+    ];
+
+    return (
+        <Box m="10px">
+            {/* HEADER */}
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Header title="EXTRA" />
+            </Box>
+
+            {/* ROW 2 */}
+            <Box
+                gridColumn="span 8"
+                gridRow="span 2"
+                backgroundColor={colors.primary[400]}
+                p={2}
+            >
+                <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                        <TableContainer component={Paper}>
+                            <Table>
+                                <TableHead>
+                                    <StyledTableRow>
+                                        <CustomTableHeaderCell colSpan={2}>SOT Header</CustomTableHeaderCell>
+                                    </StyledTableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {data.map((item, index) => (
+                                        <React.Fragment key={index}>
+                                            <StyledTableRow>
+                                                <StyledTableCell>Name</StyledTableCell>
+                                                <CustomTableCell>{item.name}</CustomTableCell>
+                                            </StyledTableRow>
+                                            <StyledTableRow>
+                                                <StyledTableCell>Age</StyledTableCell>
+                                                <CustomTableCell>{item.age}</CustomTableCell>
+                                            </StyledTableRow>
+                                        </React.Fragment>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Box>
+    );
+};
+
+export default EXTRA;
