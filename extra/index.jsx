fef46994f0b0a90b46a957aa4f71100a528c7635
DESKTOP-QBA3E74\OGS <harman.uhg@gmail.com>
2024-10-03 14:12:18 +0530

    no message

--- extra/index.jsx
+++ extra/index.jsx
@@ -1,50 +1,20 @@
-import React, { useEffect } from "react";
-import { Container, Box, Typography } from "@mui/material";
-import styled from "@emotion/styled";
-import Bg from "../../components/bg";
+function usePreventBackNavigation() {
+  const navigate = useNavigate();
 
-const RootStyle = styled("div")({
-  background: "rgb(249, 250, 251)",
-  height: "100vh",
-  display: "grid",
-  placeItems: "center",
-});
-
-const Login = () => {
   useEffect(() => {
-    const redirectToAuth = () => {
-      const authUrl = `https://authgateway1-dev/as/authorization.oauth2?response_type=code&client_id=Reg&redirect_uri=http://localhost:4000/login/&acr_values=R1_MS-Kerbose&scope=openid profile address`;
-      window.location.href = authUrl; // Automatically redirect to OAuth2 URL
+    // Push the initial state
+    window.history.pushState(null, document.title, window.location.pathname);
+
+    const handlePopState = (event) => {
+      console.log("Back button clicked, preventing navigation");
+      // Push the current state again to keep the user on the current page
+      window.history.pushState(null, document.title, window.location.pathname);
     };
 
-    // Check if the user is already redirected
-    const params = new URLSearchParams(window.location.search);
-    const code = params.get("code");
+    window.addEventListener("popstate", handlePopState);
 
-    if (!code) {
-      redirectToAuth(); // Only redirect if there's no authorization code
-    } else {
-      // Handle the received code (optional)
-      // You might want to exchange the code for tokens here
-      console.log("Authorization code received:", code);
-    }
-  }, []);
-
-  return (
-    <RootStyle>
-      <Container>
-        <Box sx={{ padding: 2, background: "#fff", borderRadius: 2, textAlign: 'center' }}>
-          <Typography variant="h4" gutterBottom>
-            Redirecting to Login...
-          </Typography>
-          <Typography variant="body1">
-            If you are not redirected automatically, please click <a href="#">here</a>.
-          </Typography>
-        </Box>
-        <Bg />
-      </Container>
-    </RootStyle>
-  );
-};
-
-export default Login;
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, [navigate]);
+}
